030bc9221ee42b407fad5dc5a42b1cb27ab97096
XApp-Studio <baumgart.guenter@gmail.com>
2012-12-18 13:58:39 +0100

    First Add

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cp -f ./wiki.conf ./externals/rstwiki/
+
+rm ./externals/rstwiki/export/Makefile
+
+rm -rf ./externals/rstwiki/export/build
+mkdir ./externals/rstwiki/export/build
+
+
+cp -f ./Makefile ./externals/rstwiki/export/
+
+cd ./externals/rstwiki/export/
+
+make data
+make html
